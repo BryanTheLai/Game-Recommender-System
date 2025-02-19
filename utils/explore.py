8d9a4b7e571e6929a5683787ec2682d090fcfec3
NotABot <135363764+BryanTheLai@users.noreply.github.com>
2025-02-19 21:31:26 +0800

    Include requirements.txt
    Essential libraries

---
+++ utils/explore.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import os
+# os.chdir('C:\\Users\\wbrya\\OneDrive\\Documents\\GitHub\\MovieLens-Recommender-System')
+# print("Current working directory:", os.getcwd())
+
