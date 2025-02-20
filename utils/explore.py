0eaec00fcbc4bd6ef9f125a0dd2379fc29209bfc
NotABot <135363764+BryanTheLai@users.noreply.github.com>
2025-02-20 23:00:46 +0800

    Explore datasets starter in utils

--- utils/explore.py
+++ utils/explore.py
@@ -7,3 +7,14 @@ import seaborn as sns
 # os.chdir('C:\\Users\\wbrya\\OneDrive\\Documents\\GitHub\\MovieLens-Recommender-System')
 # print("Current working directory:", os.getcwd())
 
+# Read JSON file
+json_file_path = 'data/raw/games_metadata.json'
+games_metadata_df = pd.read_json(json_file_path, lines=True)
+
+# Read CSV files
+games_csv = pd.read_csv('data/raw/games.csv')
+recommendations_csv = pd.read_csv('data/raw/recommendations.csv')
+users_csv = pd.read_csv('data/raw/users.csv')
+
+print("JSON DataFrame:")
+print(games_metadata_df.head())
